6a3b7902f5b320ca23de964274f6580ea0197327
dcamarro <dariocamarro@gmail.com>
2016-06-16 13:37:03 -0300

    Agrego pantalla que me permite crear carruseles. Por ahora solo se puede elegir o sacar una foto.

--- carruselinfantil/app/src/main/java/thinkup/com/carruselinfantil/AdministrarCarruselesActivity.java
+++ carruselinfantil/app/src/main/java/thinkup/com/carruselinfantil/AdministrarCarruselesActivity.java
@@ -1,16 +1,24 @@
 package thinkup.com.carruselinfantil;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class AdministrarCarruselesActivity extends DrawerAbstractActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        TextView misCarruseles = (TextView) findViewById(R.id.textoMisCarruseles);
-        misCarruseles.setText("Estoy en administrar");
+
+        Button misCarruseles = (Button) findViewById(R.id.nuevo_carrusel);
+        misCarruseles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AdministrarCarruselesActivity.this, NuevoCarruselActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     public int getContentView(){
